9f1fd0f5e1c3b47f6dc491dfe1270d7765203c39
Avisa Poshtkouhi <avisa_poshtkouhi@yahoo.com>
2019-10-31 20:34:21 +1100

    Finishing with Profile header.

--- Practice/Controllers/UserProfileController.swift
+++ Practice/Controllers/UserProfileController.swift
@@ -9,16 +9,44 @@
 import UIKit
 import Firebase
 
-class UserProfileController: UICollectionViewController {
-
+class UserProfileController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var user: User?
+    
+    let headerId = "HeaderId"
+    let cellId = "CellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.backgroundColor = .white
+        collectionView.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         
         fetchUser()
     }
     
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! UserProfileHeader
+       
+        header.user = self.user
+        
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .orange
+        return cell
+    }
 
     fileprivate func fetchUser() {
         
@@ -27,9 +55,12 @@ class UserProfileController: UICollectionViewController {
         Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
             
             guard let dictionary = snapshot.value as? [String: Any] else { return }
+           
+            self.user = User(dictionary: dictionary)
             
-            let username = dictionary["username"] as? String
-            self.navigationItem.title = username
+            self.navigationItem.title = self.user?.username
+            
+            self.collectionView.reloadData()
             
         }) { (err) in
             print("failed to fetch user:", err)
---
+++ Practice/Models/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Practice
+//
+//  Created by Avisa Poshtkouhi on 31/10/19.
+//  Copyright © 2019 Avisa Poshtkouhi. All rights reserved.
+//
+
+import Foundation
+
+
+struct User {
+    
+    let username: String
+    let profileImageUrl: String
+    
+    init(dictionary: [String: Any]) {
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+    }
+}
