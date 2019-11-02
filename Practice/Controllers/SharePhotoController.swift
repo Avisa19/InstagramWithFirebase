f02d1ed69bdcecc52c5da569758e0be69b6a829f
Avisa Poshtkouhi <avisa_poshtkouhi@yahoo.com>
2019-11-02 21:11:38 +1100

    Share post, Storage and DB.

--- Practice/Controllers/SharePhotoController.swift
+++ Practice/Controllers/SharePhotoController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SharePhotoController: UIViewController {
     
@@ -43,7 +44,58 @@ class SharePhotoController: UIViewController {
     }
     
     @objc fileprivate func handleShare() {
-        print("Continue sharing...")
+        
+        let filename = NSUUID().uuidString
+        
+        guard let image = selectedImage else { return }
+        guard let uploadedData = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        let refStorage = Storage.storage().reference().child("posts").child(filename)
+        refStorage.putData(uploadedData, metadata: nil) { (metaData, err) in
+            if let err = err {
+                print("Failed to store share image:", err)
+                return
+            }
+            
+            refStorage.downloadURL { (url, err) in
+                if let err = err {
+                    print("Failed to download imageUrl:", err)
+                    return
+                }
+               guard let imageUrl = url?.absoluteString
+                else { return }
+                 print("Successfully store share image:", imageUrl)
+                
+                self.saveToDatabaseWithImageUrl(imageUrl: imageUrl)
+            }
+        }
+        
+    }
+    
+    fileprivate func saveToDatabaseWithImageUrl(imageUrl: String) {
+        
+        guard let postImage = selectedImage else { return }
+        
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        
+        guard let caption = containerView.commentTextView.text, caption.count > 0 else { return }
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let userPostsRef = Database.database().reference().child("posts").child(uid)
+        let ref = userPostsRef.childByAutoId()
+        let values: [String: Any] = ["imageUrl": imageUrl, "caption": caption, "imageHeight": postImage.size.height, "imageWidth": postImage.size.width, "creationDate:": Date().timeIntervalSince1970]
+        
+        ref.updateChildValues(values) { (err, ref) in
+            self.navigationItem.rightBarButtonItem?.isEnabled = true
+            if let err = err {
+                print("Failed to save posts to DB:", err)
+                return
+            }
+            
+            print("Successfully saved to DB.")
+            self.dismiss(animated: true, completion: nil)
+            
+        }
+        
     }
 
 
